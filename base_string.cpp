c3a12b3bb3c189e1a9a99d04c30e7649e8a8816a
HashirGJ8842 <hameedhashirniazi@gmail.com>
2021-10-29 00:44:41 +0530

    refactor: Basic String functions

---
+++ base_string.cpp
@@ -0,0 +1,42 @@
+#include "utilities.h"
+
+uint8_t len(char* str_) {
+
+  uint8_t length;
+  for(length = 0; str_[length] != '\0'; ++length);
+
+  return length;
+
+}
+
+char* reverse(char* str_) {
+
+  uint8_t n = len(str_);
+  char* temp;
+  for(uint8_t i=0; i<n; ++i) {
+    temp[i] = str_[n-i-1];
+  }
+  temp[n] = '\0';
+  str_ = temp;
+  return str_;
+
+}
+
+
+char* to_string(const uint8_t int_) {
+
+  uint8_t length;
+  uint8_t temp = int_;
+  for(length = 0; temp != 0; temp /= 10, ++length);
+
+  char* str_;
+  temp = int_;
+
+  for(int i=0; i<length; temp /= 10, ++i) {
+    str_[i] = temp%10 + '0';
+  }
+  str_[length] = '\0';
+
+  return reverse(str_);
+
+}
---
+++ base_string.h
@@ -0,0 +1,12 @@
+#ifndef __UTILITIES_H
+#define __UTILITIES_H
+
+#include "types.h"
+
+uint8_t len(char* str_); 
+
+char* reverse(char* str_);
+
+char* to_string(const uint8_t int_);
+
+#endif
