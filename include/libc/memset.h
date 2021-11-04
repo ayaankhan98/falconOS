1063b1055b40f0e0b8f5f10f20bf4a0e84cdf609
HashirGJ8842 <hameedhashirniazi@gmail.com>
2021-11-04 18:18:36 +0530

    fix: Removed memset

--- include/libc/memset.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include<core/types.h>
-
-namespace falconOS {
-    namespace libc {
-void memset(falconOS::core::types::uint8_t *dest, falconOS::core::types::uint8_t val, falconOS::core::types::uint32_t len);
-    }
-}
